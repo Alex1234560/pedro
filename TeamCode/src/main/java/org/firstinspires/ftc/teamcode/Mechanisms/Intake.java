36ed5d7611621c83d38694e81e239b565dfa26e4
Alex1234560 <alejandrochoarteaga@gmail.com>
2026-03-18 11:48:16 -0700

    Removing Servo Helper References

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Intake.java
@@ -8,16 +8,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Intake {
     //private FunctionsAndValues FAndV;
     private DcMotorEx IntakeMotor = null;
-    private CRServo IntakeHelperCRServo = null;
-    private CRServo IntakeHelperCRServo2 = null;
+//    private CRServo IntakeHelperCRServo = null;
+//    private CRServo IntakeHelperCRServo2 = null;
 
     public void init(HardwareMap hardwareMap) {
         IntakeMotor = hardwareMap.get(DcMotorEx.class, "INTAKE");
-        IntakeHelperCRServo = hardwareMap.get(CRServo.class, "ServoHelper");
-        IntakeHelperCRServo2 = hardwareMap.get(CRServo.class, "ServoHelper2");
+//        IntakeHelperCRServo = hardwareMap.get(CRServo.class, "ServoHelper");
+//        IntakeHelperCRServo2 = hardwareMap.get(CRServo.class, "ServoHelper2");
 
-        IntakeHelperCRServo.setDirection(CRServo.Direction.REVERSE);
-        IntakeHelperCRServo2.setDirection(CRServo.Direction.REVERSE);
+//        IntakeHelperCRServo.setDirection(CRServo.Direction.REVERSE);
+//        IntakeHelperCRServo2.setDirection(CRServo.Direction.REVERSE);
     }
 
     public void start() {
@@ -26,13 +26,13 @@ public class Intake {
 
     public void intakeOn(double intake,double helper) {
         IntakeMotor.setPower(-intake);
-        IntakeHelperCRServo.setPower(-helper);
-        IntakeHelperCRServo2.setPower(-helper);
+//        IntakeHelperCRServo.setPower(-helper);
+//        IntakeHelperCRServo2.setPower(-helper);
     }
     public void intakeOff() {
         IntakeMotor.setPower(0);
-        IntakeHelperCRServo.setPower(0);
-        IntakeHelperCRServo2.setPower(0);
+//        IntakeHelperCRServo.setPower(0);
+//        IntakeHelperCRServo2.setPower(0);
     }
 
 
